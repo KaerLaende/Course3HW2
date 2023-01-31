7720195a9ae779953b1747ef1a6c3e9b747cdc54
Kaerlaende <kaerlaende@mail.ru>
2023-01-31 12:36:09 +0300

    Создание и запуск Веб Java

---
+++ src/test/java/ru/kaer/foodrecipes/FoodRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kaer.foodrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
